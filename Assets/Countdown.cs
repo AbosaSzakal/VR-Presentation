fa1f7dd1a03df5f8d28049474c672a23d1b6b202
fB222 <kissjosi182@gmail.com>
2020-06-30 18:59:25 +0200

    First version

---
+++ Assets/Countdown.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+
+    public float timeLeft = 900; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        GetComponent<Text>().text = ((int)(timeLeft / 60)).ToString() + " : " + ((int)(timeLeft % 60)).ToString();
+    }
+}
